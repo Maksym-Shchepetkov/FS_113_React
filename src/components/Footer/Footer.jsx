7ae7d0878d9669dc9253b3560443442482c4ef35
Drynkin Sergey <sergsmile29@gmail.com>
2025-03-25 18:05:12 +0200

    init m2l1

--- src/components/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-const Footer = () => {
-  return (
-    <footer>
-      <span>All right reserved &copy; 2025</span>
-    </footer>
-  );
-};
-export default Footer;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,17 @@
-const Modal = props => {
-  return <div>{props.children}</div>;
+import s from './Modal.module.css';
+const Modal = ({ children, title = 'Default modal' }) => {
+  return (
+    <div className={s.wrapper}>
+      <div className={s.content}>
+        <>
+          <h1>{title}</h1>
+          <hr />
+        </>
+        <button className={s.closeBtn}>×</button>
+        {children}
+      </div>
+    </div>
+  );
 };
+
 export default Modal;

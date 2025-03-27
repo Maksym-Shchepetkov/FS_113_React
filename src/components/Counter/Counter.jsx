24a366a4067c7c56d592e54303ae9d9152cfec5f
Drynkin Sergey <sergsmile29@gmail.com>
2025-03-27 20:12:02 +0200

    useEffect mount + update

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,13 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import s from './Counter.module.css';
 
 export const Counter = () => {
-  const [counter, setCounter] = useState(1);
+  const [counter, setCounter] = useState(0);
   const [step, setStep] = useState(10);
+
+  // 1. Виконається цей колбек лише ОДИН раз при монтуванні
+  useEffect(() => {
+    console.log('Лічильник запущено');
+  }, []);
+
+  // 2. Виконається перший раз (при монтуванні) і всі наступні рази при зміні каунтера!
+  useEffect(() => {
+    if (counter === 0) {
+      console.log('Лічильник 0, нічого не робимо');
+      return;
+    }
+    console.log(`Лічильник було оновлено!Нове значення: ${counter}`);
+  }, [counter]);
+  // 3. Виконається перший раз (при монтуванні) і всі наступні рази при зміні кроку!
+  useEffect(() => {
+    console.log(`Крок було оновлено!Нове значення: ${step}`);
+  }, [step]);
+
+  // 4. Виконається перший раз (при монтуванні) і всі наступні рази при зміні каунтера або кроку!
+  useEffect(() => {
+    console.log(`Відбувається хаос!`);
+  }, [step, counter]);
 
   const handlePlusClick = () => {
     setCounter(counter + step);
-    // console.log(counter);
   };
   const handleMinusClick = () => {
     setCounter(counter - step);
@@ -16,7 +38,6 @@ export const Counter = () => {
     setCounter(0);
     setStep(1);
   };
-  console.log('render');
 
   return (
     <div className={s.flexContainer}>

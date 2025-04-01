1d102035f51b43584ac728a1cc04dcfd21e7d76e
Drynkin Sergey <sergsmile29@gmail.com>
2025-04-01 20:04:48 +0300

    uncontrolled form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,21 @@
+import UncontrolledForm from './Forms/UncontrolledForm';
 import Header from './Header/Header';
 
 const App = () => {
+  const register = credentials => {
+    console.log('Data received!');
+    setTimeout(() => {
+      console.log('Send data...');
+    }, 1000);
+    setTimeout(() => {
+      console.log('User was registered successfully!');
+      console.log(credentials);
+    }, 2000);
+  };
   return (
     <>
       <Header />
+      <UncontrolledForm register={register} />
     </>
   );
 };

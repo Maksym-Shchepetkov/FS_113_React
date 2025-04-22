f51e0be4de98641a3268dfdc83ae3e40d7b689c9
Drynkin Sergey <sergsmile29@gmail.com>
2025-04-22 18:58:01 +0300

    init m6l1

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,10 @@ import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import './index.css';
 import 'modern-normalize';
-import { StrictMode } from 'react';
 import { Toaster } from 'react-hot-toast';
-import { BrowserRouter } from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster />
-    </BrowserRouter>
-  </StrictMode>
+  <>
+    <App />
+    <Toaster />
+  </>
 );
--- src/pages/NotFound/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-const NotFound = () => {
-  return (
-    <div>
-      <h2>Page is not found...</h2>
-    </div>
-  );
-};
-export default NotFound;
